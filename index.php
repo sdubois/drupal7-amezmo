25c03469617bddc471c965f9a8ff8d8e9cdb0196
Steven DuBois <sdubois@csh.rit.edu>
2022-08-08 22:15:01 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * The routines here dispatch control to the appropriate handler, which then
+ * prints the appropriate page.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ */
+
+/**
+ * Composer autoload
+ */
+require_once (__DIR__ . '/vendor/autoload.php');
+
+
+/**
+ * Root directory of Drupal installation.
+ */
+define('DRUPAL_ROOT', getcwd());
+
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+menu_execute_active_handler();
